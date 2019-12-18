df509a495a524fad8162377b2cca43bbb20961bc
Ahmed Abukar <ahmedabukar@Ahmeds-MacBook-Pro.local>
2019-12-18 17:28:48 -0500

    Adding Files

---
+++ src/App.js
@@ -0,0 +1,108 @@
+// Importing components from relative path 
+import React from "react";
+import Weather from "./components/weather";
+import Form from "./components/form";
+import Titles from "./components/titles";
+
+// API Key to be able to use the OpenWeatherMap API
+const Api_Key = "8d2de98e089f1c28e1a22fc19a24ef04";
+
+class App extends React.Component {
+
+  // Initialize state to undefined for all relevant data points 
+  state = {
+    temperature: undefined,
+    temp_max: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    icon: undefined,
+    speed: undefined,
+    error: undefined
+  }
+
+  // getWeather is a method we'll use to make the API call
+  // It is passed in as the loadWeather prop to Form component in the render function below
+  // Use async keyword to specify it's an asynchronous function in response to an event e
+  getWeather = async (e) => {
+    // Store city and country values based on current value in form
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    e.preventDefault();   
+    // fetch keyword for API call, await to show it's asynchronous, 
+    // URL defined at https://openweathermap.org/current
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${Api_Key}`);
+
+
+    // response stored as json in `response` variable
+    const response = await api_call.json();
+    //const response_zip = await api_call_zip.json();
+
+    console.log(response);
+    //console.log(response_zip);
+
+    if(city && country){
+      this.setState({
+        temperature: response.main.temp,
+        temp_max: response.main.temp_max,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        icon: response.weather[0].icon,
+        speed: response.wind.speed,
+        error: ""
+      })
+    }else{
+      this.setState({
+        error: "Please input search values..."
+      })
+    }
+  }
+
+  // Render function updates view whenever the state changes
+  // Components that were imported (Titles, Weather, Form) are called below as HTML tags, 
+  //   with props as attributes associated with that component
+  // Within the tag, props can be passed in to populate the component with the syntax
+  //   <componentName prop={value}>
+  // Look at the component definitions within the imported files (lines 2-5) 
+  //   to see how the props populate each component!
+  render() {
+
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form loadWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    temp_max={this.state.temp_max}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    icon={this.state.icon}
+                    speed={this.state.speed}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+// App is exported as a component which allows it to be imported by another file
+export default App
---
+++ src/components/weather.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+// Creating Weather component to display weather information
+class Weather extends React.Component{
+
+    render(){
+        // Uses regular HTML tags to build up a customized and reusable form component
+        // Defines props for city {this.props.city}, temperature {this.props.temperature}, etc.
+        //  that will be passed in when the component is called in the "render" function of another file
+        // In App.js, see this component being used with props being passed in!
+        return(
+
+            <div className="weather-info">
+                {
+                    this.props.country && this.props.city && <p className="weather__key">Location: 
+                        <span className="weather__value">  {this.props.city}, {this.props.country}</span>                    
+                    </p> 
+                }
+                
+                {
+                    this.props.temperature && <p className="weather__key">Temperature: 
+                        <span className="weather__value">  {this.props.temperature}</span>
+                    </p>
+                }
+
+                {
+                    this.props.temp_max && <p className="weather__key">Max Temperature: 
+                        <span className="weather__value">  {this.props.temp_max}</span>
+                    </p>
+                }   
+
+                {
+                    this.props.speed && <p className="weather__key">Wind Speed:  
+                        <span className="weather__value">  {this.props.speed}</span>
+                    </p>
+                }                        
+
+                {
+                    this.props.humidity && <p className="weather__key">Humidity: 
+                        <span className="weather__value">  {this.props.humidity}</span>
+                    </p>
+                }
+
+                {
+                    this.props.description && <p className="weather__key">Conditions:  
+                        <span className="weather__value">  {this.props.description}</span>
+                    </p>
+                }
+
+                
+                {
+                    this.props.icon && <p className="weather__key">Icon:  
+                        <span className="weather_value">  <img src={`http://openweathermap.org/img/wn/${this.props.icon}.png`} /> </span>
+                    </p>
+                }
+
+                {
+                    this.props.error && <p className="weather__error">{this.props.error}</p>
+                }
+        
+            </div>
+        )
+    }
+}
+
+export default Weather;
